f3803a3e55a119e70589f9aa204224a63af1592b
Gmero <2431775986@qq.com>
2023-05-11 13:30:04 +0800

    🎨优化: 样式美化

--- src/modules/textEditor/TextEditor.tsx
+++ src/modules/textEditor/TextEditor.tsx
@@ -13,7 +13,6 @@ export default function TextEditor() {
     <div class={styles['text-editor-wrapper']}>
       <div class={styles['scom-textarea']}>
         <textarea
-          class="color-text"
           placeholder={GlobalConfig.editorOpt.placeHolder}
           maxLength={GlobalConfig.editorOpt.maxLength}
           onInput={(ev) => {
